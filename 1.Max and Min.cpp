1fccf073f77fcf473dbf739a150d1e7365f840e4
Nayan Manna <nayanmanna0@gmail.com>
2025-06-12 16:40:41 +0530

    .

--- 1.Max and Min.cpp
+++ 1.Max and Min.cpp
@@ -6,14 +6,13 @@
 using namespace std;
 class Solution{
     public:
-    vector<int> minimumDeletions(vector<int> &nums){
+    int minimumDeletions(vector<int> &nums){
         int maxi = 0, mini = 0;
         for (int i = 1; i < nums.size(); i++){
             //get maxi and mini index
             if (nums[i] > nums[maxi]) maxi = i;
             if (nums[i] < nums[mini]) mini = i;
         }
-        cout<<nums[maxi] << nums[mini]<<endl;
         //remove higher index first to avoid shifting issue
         if (maxi > mini){
             nums.erase(nums.begin() + maxi);
@@ -23,15 +22,12 @@ class Solution{
             nums.erase(nums.begin() + mini);
             nums.erase(nums.begin() + maxi);
         }
-        return nums;
+        return nums.size();
     }
 };
 int main(){
     vector<int> vec = {32, 7, 9, 14, 3, 21};
     Solution s;
-    vector<int> ans = s.minimumDeletions(vec);
-    cout<<"Remaining Elements: ";
-    for (auto element: ans)
-        cout<<element<<" ";    
+    cout<<"Remaining Elements: "<<s.minimumDeletions(vec);;    
     return 0;
 }
